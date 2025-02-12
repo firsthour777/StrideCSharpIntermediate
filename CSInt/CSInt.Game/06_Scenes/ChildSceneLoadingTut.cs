11290276fc403402cb20425062a4b7359444d6e3
First Hour <firsthour777@gmail.com>
2025-02-12 03:48:45 -0500

    finished scene loading tutorial

---
+++ CSInt/CSInt.Game/06_Scenes/ChildSceneLoadingTut.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Core.Serialization;
+
+namespace CSInt_06_Scenes
+{
+   public class ChildSceneLoadingTut : SyncScript
+   {
+
+      public UrlReference<Scene> ChildSceneToLoad;
+
+      private Scene childScene;
+
+      public int timesLoaded = 0;
+
+      public override void Start()
+      {
+      }
+
+      public override void Update()
+      {
+
+         if( Input.IsKeyPressed(Keys.Space))
+         {
+            if (childScene == null)
+            {
+               // loads the scene into memory
+               childScene = Content.Load<Scene>(ChildSceneToLoad);   // you don't have to specify Scene type because we already did that with UrlReference
+
+               // if we want to load it in a particular transform position, we can do
+               childScene.Offset = new Vector3(0, timesLoaded * 0.25f, 0);   // this is the position of the child scene in the parent scene.
+
+               // sets the parent of the scene
+               childScene.Parent = Entity.Scene;   // sets it to this sceen.
+               // Entity.Scene.Children.Add(childScene);   // same thing but calls the Entity itself to load.
+
+               timesLoaded++;
+
+            }
+            else{
+
+               childScene.Parent = null;  // sets the parent to null, removing it from parent scene, which causes it to be unrendered, but still in memory.
+               Content.Unload(childScene);  // removes the child scene from memory.
+               childScene = null;   // does a similar thing, removes everything
+
+
+            }
+
+         }
+      }
+   }
+}
---
+++ CSInt/CSInt.Game/06_Scenes/SceneLoadingTut.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Core.Serialization;
+
+namespace CSInt_06_Scenes
+{
+   public class SceneLoadingTut : SyncScript
+   {
+      public UrlReference<Scene> sceneToLoad;
+
+      public override void Start()
+      {
+         // Initialization of the script.
+      }
+
+      public override void Update()
+      {
+         if (Input.IsKeyPressed(Keys.Z))
+         {
+
+            // In order to unload the root secene, you can't just expect Entity.Scene to be the root.
+            // It must be the SceneSystem.
+            Content.Unload(SceneSystem.SceneInstance.RootScene);
+
+            SceneSystem.SceneInstance.RootScene = Content.Load<Scene>(sceneToLoad); // this will load the scene then put it as root all in one.
+
+         }
+      }
+   }
+}
