78b3d856436de60c219694dc69fb272897c5112f
First Hour <firsthour777@gmail.com>
2025-02-13 14:05:12 -0500

    finished first person movement camera tutorial

---
+++ CSInt/CSInt.Game/09_FirstPersonCamera/FirstPersonCamTut.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace CSInt_09_FirstPersonCamera
+{
+   public class FirstPersonCamTut : SyncScript
+   {
+
+      private bool isActive = true; // is your camera active, meaning not paused with mouse screen
+
+      public float MouseSpeed = 0.5f;  // This gets applied to camera rotation as the mouse moves in an FPS
+
+      private Vector3 cameraRotation;     
+
+      public bool isXAxisInverted;     // set in editor if you want it to be inverted, will likely be read in from settings later on.
+      public bool isYAxisInverted;
+
+      public float MaxCameraAngleDegrees = 50.0f;  // Prevents continued rotation, you can set it in the editor.
+      public float MinCameraAngleDegrees = -50.0f;
+
+      private float maxCameraAngleRadians;   // holder for when we convert to radians
+      private float minCameraAngleRadians;
+
+      public Entity CameraPivotPoint;  // this is the camera's pivot point which you should have for any type of camera on model situation.
+
+      public CharacterComponent FirstPersonCharacterComponent; // set in editor, this is the component which ahs the colliders
+
+      public override void Start()
+      {
+
+         Game.IsMouseVisible = false;  // sets state to not show the mouse.
+
+         cameraRotation = Entity.Transform.RotationEulerXYZ;   // You must set this to get the correct camera position as it is in the editor.
+
+         maxCameraAngleRadians = MathUtil.DegreesToRadians(MaxCameraAngleDegrees);  // conversion to Radians.
+         minCameraAngleRadians = MathUtil.DegreesToRadians(MinCameraAngleDegrees);
+
+
+
+      }
+
+      public override void Update()
+      {
+         if (Input.HasKeyboard)
+         {
+            if (Input.IsKeyPressed(Keys.Escape))
+            {
+               isActive = !isActive;   // pauses fps camera and stops looking around to instead use the mouse.
+               Game.IsMouseVisible = !isActive;
+               Input.UnlockMousePosition();
+            }
+
+         }
+
+         if (isActive)
+         {
+
+            Input.LockMousePosition();    // locks mouse so it doesn't appear on the screen.
+
+            var mouseMovement = Input.MouseDelta * MouseSpeed;    // gets the delta version of the mouse speed.
+
+            cameraRotation.Y -= mouseMovement.X * ConvertInversionBool(isXAxisInverted);   // x movement is rotating the y axis, its negative because it most go the opposite way as if you are looking that way.
+            cameraRotation.X += mouseMovement.Y * ConvertInversionBool(isYAxisInverted);  // this does not need negative as its the opposite of the y axis.
+
+            cameraRotation.X = MathUtil.Clamp(cameraRotation.X, minCameraAngleRadians,  maxCameraAngleRadians);   // had to change this in the tutorial this clamps and prevents over rotation.
+
+
+            // Entity.Transform.Rotation = Quaternion.RotationY(cameraRotation.Y);     // sets the rotation for Y, which is looking along the x axis movement.
+            //commented out the above because ow that we have a character component, we want to rotate the character component instead
+            FirstPersonCharacterComponent.Orientation = Quaternion.RotationY(cameraRotation.Y);   // this rotates the character component instead of the entire entity, which is what we want.
+            CameraPivotPoint.Transform.Rotation = Quaternion.RotationX(cameraRotation.X);    // sets the rotation for X, based on the pivot point, otherwise you move the entire mesh.
+            // you don't want to move the entire mesh on the Y axis as it will rotate the entire model, but its fine for X axis because that makes sense.
+            // it might make sense if the model is completely symetrical on all sides, like a sphere, but idk
+
+         }
+      }
+
+
+      public int ConvertInversionBool(bool isInverted)
+      {
+         if (isInverted)
+         {
+            return -1;
+         }
+         else
+         {
+            return 1;
+         }
+      }
+
+
+
+
+   }
+
+}
---
+++ CSInt/CSInt.Game/09_FirstPersonCamera/PlayerMoverTut.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace CSInt_09_FirstPersonCamera
+{
+   public class PlayerMoverTut : SyncScript
+   {
+      public Vector3 MovementSpeed = new Vector3(3, 0, 4);  // base movement speed, x is left/right, z is forward/backward
+      // could add y here for something like jumping or levitation, but not for this tutorial.
+
+      public CharacterComponent FirstPersonCharacterComponent; // set in editor, this is the component which has the colliders
+
+      public override void Start()
+      {
+         // Initialization of the script.
+      }
+
+      public override void Update()
+      {
+         var velocity = new Vector3(); // the velocity for how much we are going to move an entity.
+
+         if (Input.HasKeyboard)
+         {
+
+            // WASD movement
+            if (Input.IsKeyDown(Keys.W))
+            {
+               velocity.Z++;
+            }
+
+            if (Input.IsKeyDown(Keys.S))
+            {
+               velocity.Z--;
+            }
+
+            if (Input.IsKeyDown(Keys.A))
+            {
+               velocity.X++;
+            }
+
+            if (Input.IsKeyDown(Keys.D))
+            {
+               velocity.X--;
+            }
+
+            velocity.Normalize();   // Normalize it so we don't move faster diagonally.
+
+            velocity *= MovementSpeed; // multiply by the base speed
+
+            
+            velocity = Vector3.Transform(velocity, Entity.Transform.Rotation);   // transform movement into the look direction
+
+            FirstPersonCharacterComponent.SetVelocity(velocity);  // does the movement using the special Character collider
+
+
+
+         }
+      }
+   }
+}
